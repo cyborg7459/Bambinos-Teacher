6de90ec255efe84b333fd4bc8da0bee005d0866f
Averek <singhavanish2003@gmail.com>
2021-10-06 10:55:09 +0530

    homepage commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './App.css';
-import {Route, Switch} from 'react-router-dom';
+import {BroweserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import SignInPage from './pages/sign-in/sign-in.component';
 import SignUpPage from './pages/sign-up/sign-up.component';
 import OnboardingPage from './pages/onboarding/onboarding.component';
 import Loader from './components/loader/loader.component';
+import { Homepage } from './pages/homepage/homepage.component';
 
 class App extends React.Component{
   render() {
     return (
       <div>
+      <Router>
+        <Homepage/>
         <Switch>
           <Route exact path="/signin" component={SignInPage} />
           <Route exact path="/signup" component={SignUpPage} />
           <Route path="/onboard" component = {OnboardingPage} />
           <Route exact path="/loader" component={Loader}/>
         </Switch>
+      </Router>
       </div>
     )
   }
